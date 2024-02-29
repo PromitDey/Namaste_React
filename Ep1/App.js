b0f15ec2a45c7b372373143f304c284f950ee27c
PromitDey <promitdey140@gmail.com>
2024-02-29 21:46:38 +0530

    ep2

--- Ep1/App.js
+++ Ep1/App.js
@@ -1,22 +1,10 @@
-/*
-<div id = "parent">
-    <div id = "child">
-        <h1>I am H1 nested tag</h1>
-        <h2>I am H1 nested tag</h2>
-    </div>
-    <div id = "child2">
-        <h1>I am H1 nested tag</h1>
-        <h2>I am H1 nested tag</h2>
-    </div>
-</div>
-
-creating this above nested thing in JS
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {id: "heading1"}, "I am H1 nested tag"),
-    React.createElement("h2", {id: "heading2"}, "I am H2 nested tag"),
+    React.createElement("h1", {id: "heading1"}, "Less GOooooo "),
+    React.createElement("h2", {id: "heading2"}, "I am H2 nested tag ."),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {id: "heading1"}, "I am H1 nested tag of child2"),
